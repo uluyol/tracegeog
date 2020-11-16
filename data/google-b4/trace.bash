6fcbbb86671abbe53709f14b59334036e7540c45
Muhammed Uluyol <uluyol0@gmail.com>
2020-11-15 19:34:25 -0500

    data/google-b4: list source and add comments

--- data/google-b4/trace.bash
+++ data/google-b4/trace.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+#
+# Google's B4 backbone (2018)
+#
+# Source: https://www2.cs.duke.edu/courses/fall18/compsci514/readings/b4after-sigcomm18.pdf
 
 set -e
 
+# Automatically trace nodes
 # ../../tracegeog trace-nodes \
 #     -i orig.png \
 #     -o xygraph.json \
@@ -9,6 +14,7 @@ set -e
 #     -node-color-accuracy 0.7 \
 #     -max-node-count 18
 
+# Automatically trace links (unused)
 # ../../tracegeog trace-links \
 #     -i orig.png \
 #     -g xygraph.json \
@@ -20,12 +26,16 @@ set -e
 #     -line-node-dist 10 \
 #     -line-width 1
 
+# Manually specify links
+
+# Visualize links
 # ../../tracegeog vis \
 #     -i orig.png \
 #     -g xygraph-manual-links.json \
 #     -png xygraph.png \
 #     -overlaypng overlayed.png
 
+# Invert projected data to lat, lon pairs
 ../../tracegeog unproj \
     -g xygraph-manual-links.json \
     -extra-margin-left 330 \
@@ -33,6 +43,7 @@ set -e
     -equator-y 467 \
     -o geograph-manual-links.json
 
+# Replot on map to compare against source data
 ../../scripts/plotgeo.py \
     geograph-manual-links.json \
     redrawn.pdf
